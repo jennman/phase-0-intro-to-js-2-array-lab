17371f17fd1667c3d03421e5ab6c70c5c62653e8
Jennifer Man <jenniferman702@gmail.com>
2022-09-20 09:33:37 -0400

    commit tree b19ecacdc73224f626491240e0d934b654b1286e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+cats;
+
+function destructivelyAppendCat(name){
+   cats.push(name);
+   return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    cats;
+}
+
+function appendCat(name){
+    const newCats = [name, ...cats];
+    return newCats;
+}
